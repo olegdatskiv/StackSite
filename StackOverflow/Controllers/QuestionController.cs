6666cdd790570ed0c0246b3de60e2fc244bb3b02
olegdatskiv <olegdatskiv@gmail.com>
2017-07-13 14:29:20 +0300

    add new question controller

---
+++ StackOverflow/Controllers/QuestionController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackOverflow.Controllers
+{
+    public class QuestionController : Controller
+    {
+        // GET: Question
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            using (UserAccountEntities db = new UserAccountEntities())
+            {
+                return View(db.Questions.ToList());
+            }
+        }
+
+    }
+}
